2f0491a3874cd983d8ae70fa6f159e336dbcf2fa
leedd87 <leedav87@gmail.com>
2022-06-09 00:28:55 -0300

    scroll to top

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/index.css";
 import App from "./App";
+import ScrollToTop from "./helpers/ScrollToTop";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<BrowserRouter>
+		<ScrollToTop />
 		<App />
 	</BrowserRouter>
 );
