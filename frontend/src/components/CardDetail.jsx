c58755f015dba50aba295e28b459ced3f44d6f76
leedd87 <leedav87@gmail.com>
2022-06-16 07:35:03 -0300

    sprint 2

--- frontend/src/components/CardDetail.jsx
+++ frontend/src/components/CardDetail.jsx
@@ -4,18 +4,21 @@ import { Link as LinkRouter } from "react-router-dom";
 
 const CardDetail = ({ city }) => {
 	return (
-		<div className="d-flex flex-row card-detail p-5 rounded">
+		<div className="d-flex flex-column flex-md-row card-detail p-5 rounded w-100">
 			<div>
-				<img src={city.image} alt={city.city} className="img-card" />
-				<img src={city.image} alt={city.city} className="img-card" />
+				<img
+					src={city.image}
+					alt={city.city}
+					className="img-card rounded"
+				/>
 			</div>
-			<div className="mx-4">
+			<div className="mx-4 d-flex flex-column align-items-center justify-content-center detail-text-container p-3 my-3 rounded">
 				<div>
 					<h1>{city.name}</h1>
 					<p>{city.description}</p>
 				</div>
-				<LinkRouter to="/cities" className="linkRouter mx-2">
-					Cities
+				<LinkRouter to="/cities" className="btn-detail mt-2 linkRouter">
+					Back to cities
 				</LinkRouter>
 			</div>
 		</div>
