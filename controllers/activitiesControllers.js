ea08ae3e5967d1dfb317adf54ac724926d84ed8f
leedd87 <leedav87@gmail.com>
2022-07-05 09:56:34 -0300

    activities controllers

--- controllers/activitiesControllers.js
+++ controllers/activitiesControllers.js
@@ -22,5 +22,80 @@ const activitiesControllers = {
 
 	addActivity: async (req, res) => {
 		const { name, image, itinerary } = req.body.data;
+
+		let activity;
+		let error = null;
+		try {
+			activity = await new Activity({
+				name: name,
+				image: image,
+				itinerary: itinerary,
+			}).save();
+		} catch (err) {
+			error = err;
+		}
+		res.json({
+			response: error ? "ERROR" : activity,
+			success: error ? false : true,
+			error: error,
+		});
+	},
+
+	modifyActivity: async (req, res) => {
+		const id = req.params.id;
+		const activity = req.body.data;
+		let activitydb;
+		let error = null;
+		try {
+			activitydb = await Activity.findOneAndUpdate({ _id: id }, activity, {
+				new: true,
+			});
+		} catch (err) {
+			error = err;
+		}
+		res.json({
+			response: error ? "ERROR" : activitydb,
+			success: error ? false : true,
+			error: error,
+		});
+	},
+
+	removeActivity: async (req, res) => {
+		const id = req.params.id;
+		let activity;
+		let error = null;
+		try {
+			activity = await Activity.findByIdAndDelete({ _id: id });
+		} catch (err) {
+			error = err;
+		}
+		res.json({
+			response: error ? "ERROR" : activity,
+			success: error ? false : true,
+			error: error,
+		});
+	},
+
+	findActivityFromItinerary: async (req, res) => {
+		let activityId = req.params.id;
+		let activities;
+		let error = null;
+		try {
+			activities = await Activity.find({ activity: activityId }).populate(
+				"itinerary",
+				{
+					itineraryName: 1,
+				}
+			);
+		} catch (err) {
+			error = err;
+		}
+		res.json({
+			response: error ? "ERROR" : activities,
+			success: error ? false : true,
+			error: error,
+		});
 	},
 };
+
+module.exports = activitiesControllers;
