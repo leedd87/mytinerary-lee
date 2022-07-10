0782620f04bc47a640141e283d06e92480192c6d
leedd87 <leedav87@gmail.com>
2022-07-10 12:37:17 -0300

    frontend

--- frontend/src/redux/actions/activitiesActions.js
+++ frontend/src/redux/actions/activitiesActions.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const url = "http://localhost:4000/";
+// const url = "http://localhost:4000/";
+const url = "https://mytinerary-lee-backend.herokuapp.com/";
 
 const activitiesActions = {
 	findActivityFromItinerary: (id) => {
